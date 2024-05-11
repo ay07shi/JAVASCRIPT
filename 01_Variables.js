f64ca3b5444aa274a1b0240b4b5e7107afe87055
Ayushi <155347350+ay07shi@users.noreply.github.com>
2024-05-11 16:10:38 +0530

    Add files via upload

---
+++ 01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 14455
+let accountEmail = "ayushi@goggle.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2 // not allowed  
+
+console.log(accountId);
+accountEmail ="hc@hc.com"
+accountCity ="bengluru"
+
+console.table([accountId,accountEmail , accountPassword , accountCity]) 
